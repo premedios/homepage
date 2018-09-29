e57d1556dc0c4ea26c5d73c6b5bc230b1ab0a909
Pedro Remedios <pedro.remedios@gmail.com>
2018-09-29 21:36:18 +0100

    Proper title and favicon. Hero formatting changes

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -15,8 +15,8 @@ export default class HeroSection extends Component {
   render() {
     return (
       <Jumbotron fluid className="text-center d-flex flex-column justify-content-center text-white mb-0" style={jumbotronStyle}>
-        <h1 className="display-4 font-weight-normal mb-4">PEDRO REMEDIOS</h1>
-        <h4 className="font-weight-light display-sm-4 display-5">Application Development Done Right!</h4>
+        <h1 className="display-3 font-weight-bold mb-4">PEDRO REMEDIOS</h1>
+        <h4 className="font-weight-light display-sm-4 display-4">Application Development Done Right!</h4>
       </Jumbotron>
     )
   }
