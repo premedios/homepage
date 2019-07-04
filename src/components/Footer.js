9964cf26edee7e4fed27cc05d8dc029a1c9d8617
Pedro Remedios <pedro.remedios@gmail.com>
2019-07-04 23:39:24 +0100

    Changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,52 +1,78 @@
-import React, { Component } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope, faMapMarker, faPhone } from '@fortawesome/free-solid-svg-icons';
-import { faFacebookSquare, faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faMapMarker, faPhone } from "@fortawesome/free-solid-svg-icons";
+import { faFacebookSquare, faGithub, faTwitter } from "@fortawesome/free-brands-svg-icons";
 
 export default class Footer extends Component {
-  render() {
-    return (
-      <footer id="footer-section" className="py-4 bg-dark text-white">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-6">
-              <h5 className="mb-4 font-weight-light">About Me</h5>
-              <p className="font-weight-light about-text">iOS and Web developer with over 20 years professional development experience. Hobbies include: AI, Car Tuning, World of Warcraft, Bodybuilding and Cinema.</p>
-            </div>
-            <div className="col-md-6">
-              <h5 className="mb-4 font-weight-light">Contact Information</h5>
-              <div className="row mb-3">
-                <div className="col-1"><FontAwesomeIcon icon={faEnvelope} fixedWidth /></div>
-                <div className="col-11">pedro.remedios@gmail.com</div>
-              </div>
-              <div className="row">
-                <div className="col-1"><FontAwesomeIcon icon={faMapMarker} fixedWidth /></div>
-                <div className="col-11">
-                  <address>
-                    Rua Abel Manta, 14 R/C Esq. <br/>
-                    Figueirinha <br/>
-                    2780-174 Oeiras <br/>
-                    PORTUGAL <br/>
-                  </address>
-                </div>
-              </div>
-              <div className="row mb-4">
-                <div className="col-1"><FontAwesomeIcon icon={faPhone} fixedWidth /></div>
-                <div className="col-11">+351912140773</div>
-              </div>
-              <ul className="nav social">
-                <li className="nav-item">
-                  <a href="https://www.facebook.com/pedroremedios" target="_blank" rel='noopener noreferrer' className="nav-link"><FontAwesomeIcon icon={faFacebookSquare} size="2x" fixedWidth /></a>
-                </li>
-                <li className="nav-item">
-                  <a href="https://www.github.com/premedios" target="_blank" rel='noopener noreferrer' className="nav-link"><FontAwesomeIcon icon={faGithub} size="2x" fixedWidth/></a>
-                </li>
-                <li className="nav-item">
-                  <a href="https://www.twitter.com/prmobiledev" rel='noopener noreferrer' className="nav-link"><FontAwesomeIcon icon={faTwitter} size="2x" fixedWidth /></a>
-                </li>
-              </ul>
-            </div>
-            {/* <div className="col-md-4">
+    render() {
+        return (
+            <footer id="footer-section" className="py-4 bg-dark text-white">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-6">
+                            <p className=" h5 mb-4 font-weight-light">About Me</p>
+                            <p className="font-weight-light about-text">
+                                iOS and Web developer with over 20 years professional development experience. Hobbies
+                                include: AI, Car Tuning, World of Warcraft, Bodybuilding and Cinema.
+                            </p>
+                        </div>
+                        <div className="col-md-6">
+                            <h5 className="mb-4 font-weight-light">Contact Information</h5>
+                            <div className="row mb-3">
+                                <div className="col-1">
+                                    <FontAwesomeIcon icon={faEnvelope} fixedWidth />
+                                </div>
+                                <div className="col-11">pedro.remedios@gmail.com</div>
+                            </div>
+                            <div className="row">
+                                <div className="col-1">
+                                    <FontAwesomeIcon icon={faMapMarker} fixedWidth />
+                                </div>
+                                <div className="col-11">
+                                    <address>
+                                        Rua Abel Manta, 14 R/C Esq. <br />
+                                        Figueirinha <br />
+                                        2780-174 Oeiras <br />
+                                        PORTUGAL <br />
+                                    </address>
+                                </div>
+                            </div>
+                            <div className="row mb-4">
+                                <div className="col-1">
+                                    <FontAwesomeIcon icon={faPhone} fixedWidth />
+                                </div>
+                                <div className="col-11">+351912140773</div>
+                            </div>
+                            <ul className="nav social">
+                                <li className="nav-item">
+                                    <a
+                                        href="https://www.facebook.com/pedroremedios"
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        className="nav-link">
+                                        <FontAwesomeIcon icon={faFacebookSquare} size="2x" fixedWidth />
+                                    </a>
+                                </li>
+                                <li className="nav-item">
+                                    <a
+                                        href="https://www.github.com/premedios"
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        className="nav-link">
+                                        <FontAwesomeIcon icon={faGithub} size="2x" fixedWidth />
+                                    </a>
+                                </li>
+                                <li className="nav-item">
+                                    <a
+                                        href="https://www.twitter.com/prmobiledev"
+                                        rel="noopener noreferrer"
+                                        className="nav-link">
+                                        <FontAwesomeIcon icon={faTwitter} size="2x" fixedWidth />
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        {/* <div className="col-md-4">
               <h5 className="mb-4 font-weight-light">Drop me a message:</h5>
               <form>
                 <div className="row">
@@ -71,9 +97,9 @@ export default class Footer extends Component {
                 <input type="button" value="Submit" className="btn btn-primary btn-block" />
               </form>
             </div> */}
-          </div>
-        </div>
-      </footer>
-    )
-  }
+                    </div>
+                </div>
+            </footer>
+        );
+    }
 }
